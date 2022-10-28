1a3997ebd0ebc5f8b0d386b6020c74b39abe031d
CG-B05S <104055011+CG-B05S@users.noreply.github.com>
2022-10-28 13:02:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import UseRef from './UseRef';
+import UseMemo from './UseMemo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <UseRef/>
+      <UseMemo/>
+    </div>
+  );
+}
+
+export default App;
